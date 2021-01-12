8d8f0536659ec8da10ba4f0da2ae0a9fc9ff93ec
PabloGlez <pabloarguelles97@gmail.com>
2021-01-12 21:02:38 +0100

    token v2

---
+++ tika-core/src/test/java/org/apache/tika/extractor/ExtractorTest.java
@@ -0,0 +1,15 @@
+package org.apache.tika.extractor;
+
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+
+public class ExtractorTest {
+
+    public void test() {
+        Metadata m = n
+    }
+
+
+
+
+}
